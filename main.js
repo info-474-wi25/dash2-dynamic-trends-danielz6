43f0bc0cfdb375909387539b89c387a6990eea6e
danielz6 <danielzhang606@gmail.com>
2025-02-21 17:03:11 -0800

    updates

--- main.js
+++ main.js
@@ -11,52 +11,41 @@ const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must 
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-const svg2_RENAME = d3.select("#lineChart2")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-
 // (If applicable) Tooltip element for interactivity
 // const tooltip = ...
 
 // 2.a: LOAD...
-d3.csv("YOUR_CSV_NAME.csv").then(data => {
+d3.csv("aircraft_incidents.csv").then(data => {
+    console.log('data', data)
     // 2.b: ... AND TRANSFORM DATA
-
+    data.forEach(d => {
+        d.Year = d["Row Labels"];
+        delete d["Row Labels"];  
+    });
+    data.forEach(d => {
+        d.Count = d["Count of Accident_Number"];
+        delete d['Count of Accident_Number'];
+    });
+    
     // 3.a: SET SCALES FOR CHART 1
+    const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.Year))
+    .range([0, width - margin.left - margin.right]);
 
+    const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.Accidents) + 5]) 
+    .range([height - margin.top - margin.bottom, 0]);
 
     // 4.a: PLOT DATA FOR CHART 1
 
 
     // 5.a: ADD AXES FOR CHART 1
 
-
     // 6.a: ADD LABELS FOR CHART 1
 
 
     // 7.a: ADD INTERACTIVITY FOR CHART 1
     
-
-    // ==========================================
-    //         CHART 2 (if applicable)
-    // ==========================================
-
-    // 3.b: SET SCALES FOR CHART 2
-
-
-    // 4.b: PLOT DATA FOR CHART 2
-
-
-    // 5.b: ADD AXES FOR CHART 
-
-
-    // 6.b: ADD LABELS FOR CHART 2
-
-
-    // 7.b: ADD INTERACTIVITY FOR CHART 2
 
 
 });
