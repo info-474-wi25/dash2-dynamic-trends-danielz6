02b12883e991ed248e3c28788c12fb2fba72d11d
danielz6 <danielzhang606@gmail.com>
2025-02-28 17:27:53 -0800

    final

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-// 1: SET GLOBAL VARIABLES
+// Set up the SVG canvas dimensions
 const margin = { top: 50, right: 30, bottom: 60, left: 70 };
 const width = 900 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
-// Create SVG containers for both charts
-const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+// Select the existing SVG in index.html
+const svg = d3.select("#lineChart1")
+    .append("g") 
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // (If applicable) Tooltip element for interactivity
@@ -16,27 +13,28 @@ const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must 
 
 // 2.a: LOAD...
 d3.csv("aircraft_incidents.csv").then(data => {
-    console.log('data', data)
-    // 2.b: ... AND TRANSFORM DATA
+    console.log("Loaded Data:", data);
+    console.log("Column Names:", Object.keys(data[0]));
+
+//2.b: ... AND TRANSFORM DATA
     data.forEach(d => {
-        d.Year = d["Row Labels"];
-        delete d["Row Labels"];  
+        d.Year = +d["Row Labels"]; 
+        d.Count = +d["Count of Accident_Number"]; 
     });
-    data.forEach(d => {
-        d.Count = d["Count of Accident_Number"];
-        delete d['Count of Accident_Number'];
-    });
-    
+    data.sort((a, b) => a.Year - b.Year);
+
+    console.log("Years:", data.map(d => d.Year)); 
+    console.log("Counts:", data.map(d => d.Count));  
+
     // 3.a: SET SCALES FOR CHART 1
     const xScale = d3.scaleLinear()
-    .domain(d3.extent(data, d => d.Year))
-    .range([0, width - margin.left - margin.right]);
+        .domain(d3.extent(data, d => d.Year))
+        .range([0, width]);
 
     const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.Accidents) + 5]) 
-    .range([height - margin.top - margin.bottom, 0]);
+        .domain([0, d3.max(data, d => d.Count) / 8])
+        .range([height, 0]);
 
-    // 4.a: PLOT DATA FOR CHART 1
     svg.append("g")
         .attr("transform", `translate(0,${height})`)
         .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
@@ -44,13 +42,11 @@ d3.csv("aircraft_incidents.csv").then(data => {
     svg.append("g")
         .call(d3.axisLeft(yScale));
 
-    // Define the line generator
     const line = d3.line()
         .x(d => xScale(d.Year))
         .y(d => yScale(d.Count))
         .curve(d3.curveMonotoneX);
-
-    // Append the line path
+    
     svg.append("path")
         .datum(data)
         .attr("fill", "none")
@@ -58,14 +54,31 @@ d3.csv("aircraft_incidents.csv").then(data => {
         .attr("stroke-width", 2)
         .attr("d", line);
 
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .attr("text-anchor", "middle")
+        .text("Year");
 
-    // 5.a: ADD AXES FOR CHART 1
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 15)
+        .attr("text-anchor", "middle")
+        .text("Number of Incidents");
 
-    // 6.a: ADD LABELS FOR CHART 1
-
-
-    // 7.a: ADD INTERACTIVITY FOR CHART 1
+    const legend = svg.append("g")
+        .attr("class", "legend")
+        .attr("transform", `translate(${width - 150}, 10)`);
     
-
+    legend.append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", "steelblue");
+    
+    legend.append("text")
+        .attr("x", 20)
+        .attr("y", 10)
+        .text("Aircraft Incidents");
 
 });
